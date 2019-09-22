9ff3684deee0eeaaa559c35b9e94587fe974f3ef
yugimamidi <yugi.mamidi01@gmail.com>
2019-09-22 10:32:36 -0500

    updated with validation

--- iosui.js
+++ iosui.js
@@ -1,10 +1,19 @@
 function calc(){
     const f1 = document.getElementById("total").value;
-    const f2 = document.getElementById("rate").value;
+    const f2 = document.getElementById("rate").value; 
+    if(f1.length == 0  || f2.length == 0 || f1.match(/[a-z]/i))
+    {
+     document.getElementById("tip").innerHTML = "$"+f1.length.toFixed(2);
+     document.getElementById("tax").innerHTML = "$"+f2.length.toFixed(2);
+     document.getElementById("grandt").innerHTML="Entered values are not valid";
+    }
+    else
+    {
     const ti = parseFloat(f2)*parseFloat(f1)/100;
-    document.getElementById("tip").innerHTML = ti.toFixed(2);
+    document.getElementById("tip").innerHTML = "$"+ti.toFixed(2);
     const ta = (5.5/100)*parseFloat(f1);
-    document.getElementById("tax").innerHTML = ta.toFixed(2);
+    document.getElementById("tax").innerHTML = "$"+ta.toFixed(2);
     const grndt = ti+ta+parseFloat(f1);
-    document.getElementById("grandt").innerHTML=grndt.toFixed(2);   
+    document.getElementById("grandt").innerHTML="$"+grndt.toFixed(2);  
+    }
 }
